e3867f854fff59a6c389b0cc5a22aa8c2662315a
Dmitry <DmitryChukhnenko@yandex.ru>
2024-08-28 15:48:09 +0300

    HW 5

--- app/src/Controllers/Api/Login.php
+++ app/src/Controllers/Api/Login.php
@@ -2,13 +2,19 @@
 
 namespace Controllers\Api;
 
+use Models\User;
 use Request\Post;
-use Database;
+use Database\DB;
 
 class Login {
     public function postRequest(Post $request) : string {
-        
+        if (!$request->has('login') || !$request->has('password')) return '{error: Incorrect data}';
+        $user = User::getUserById($request->has('login'));
+        if ($request->get('password') !== $user->password) return '{error: Incorrect password}';
 
-        return '';
+        $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+        $token = substr(str_shuffle($permitted_chars), 0, 10);
+        DB::getInstance()->query("UPDATE users SET token = {$token} WHERE id={$user->id};");
+        return '{token: ' . $token . '}';
     }
 }
